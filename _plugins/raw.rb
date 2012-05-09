68d6473bcb6495b845e18f797036ed2562671dcc
Rob Figueiredo <robfig@gmail.com>
2012-05-08 21:00:52 -0400

    Attempt to allow a raw tag

---
+++ _plugins/raw.rb
@@ -0,0 +1,20 @@
+module Jekyll
+  class RawTag < Liquid::Block
+    def parse(tokens)
+      @nodelist ||= []
+      @nodelist.clear
+
+      while token = tokens.shift
+        if token =~ FullToken
+          if block_delimiter == $1
+            end_tag
+            return
+          end
+        end
+        @nodelist << token if not token.empty?
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('raw', Jekyll::RawTag)
