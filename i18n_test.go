d1f2741fee18b1a8bcedc631ae3c98d7c0a049b7
tmbrggmn <tmbrggmn@gmail.com>
2012-11-14 19:50:01 +0100

    Added test case and benchmark for Message() with folding and arguments

--- i18n_test.go
+++ i18n_test.go
@@ -38,6 +38,9 @@ func TestI8nMessage(t *testing.T) {
 	if message := Message("en", "arguments.hex", 1234567, 1234567); message != "The number 1234567 in hexadecimal notation would be 12d687" {
 		t.Errorf("Message 'arguments.hex' for locale 'en' (%s) does not have the expected value", message)
 	}
+	if message := Message("en", "folded.arguments", 12345); message != "Rob is 12345 years old" {
+		t.Errorf("Message 'folded.arguments' for locale 'en' (%s) does not have the expected value", message)
+	}
 
 	// TODO: re-enable this test once files merging has been implemented
 	/*if message := Message("en", "greeting2"); message != "Yo!" {
@@ -64,5 +67,12 @@ func BenchmarkI8nMessageWithArguments(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		TRACE = log.New(ioutil.Discard, "", 0)
 		Message("en", "arguments.string", "Vincent Hanna")
+	}
+}
+
+func BenchmarkI8nMessageWithFoldingAndArguments(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		TRACE = log.New(ioutil.Discard, "", 0)
+		Message("en", "folded.arguments", 12345)
 	}
 }
