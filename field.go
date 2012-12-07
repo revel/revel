985fb9e4bbe8c1e3102210188cc7cb8c1b12fb47
Rob Figueiredo <robfig@gmail.com>
2012-12-06 23:44:24 -0500

    Extract the {{field}} helper to its own file and make it consistent
    Previously field.Value returned the flashed value -- in fact there are
    use cases where the developer needs the current value (e.g. from
    RenderArgs, not from the flash).  So, we adopt the Play! naming
    convention and make the current value accessible.
    TODO: make {{radio}} and {{option}} support .Value()

---
+++ field.go
@@ -0,0 +1,72 @@
+package rev
+
+import (
+	"reflect"
+	"strings"
+)
+
+// Field represents a data fieid that may be collected in a web form.
+type Field struct {
+	Name       string
+	Error      *ValidationError
+	renderArgs map[string]interface{}
+}
+
+func NewField(name string, renderArgs map[string]interface{}) *Field {
+	err, _ := renderArgs["errors"].(map[string]*ValidationError)[name]
+	return &Field{
+		Name:       name,
+		Error:      err,
+		renderArgs: renderArgs,
+	}
+}
+
+// Returns an identifier suitable for use as an HTML id.
+func (f *Field) Id() string {
+	return strings.Replace(f.Name, ".", "_", -1)
+}
+
+// Returned the flashed value of this field.
+func (f *Field) Flash() string {
+	v, _ := f.renderArgs["flash"].(map[string]string)[f.Name]
+	return v
+}
+
+// Returned the flashed value of this field as a list.
+func (f *Field) FlashArray() []string {
+	v := f.Flash()
+	if v == "" {
+		return []string{}
+	}
+	return strings.Split(v, ",")
+}
+
+// Return the current value of this field.
+func (f *Field) Value() interface{} {
+	pieces := strings.Split(f.Name, ".")
+	answer, ok := f.renderArgs[pieces[0]]
+	if !ok {
+		return ""
+	}
+
+	val := reflect.ValueOf(answer)
+	for i := 1; i < len(pieces); i++ {
+		if val.Kind() == reflect.Ptr {
+			val = val.Elem()
+		}
+		val = val.FieldByName(pieces[i])
+		if !val.IsValid() {
+			return ""
+		}
+	}
+
+	return val.Interface()
+}
+
+// Return ERROR_CLASS if this field has a validation error, else empty string.
+func (f *Field) ErrorClass() string {
+	if f.Error != nil {
+		return ERROR_CLASS
+	}
+	return ""
+}
--- template.go
+++ template.go
@@ -34,26 +34,6 @@ type Template interface {
 	Render(wr io.Writer, arg interface{}) error
 }
 
-type Field struct {
-	Name, Value string
-	Error       *ValidationError
-}
-
-func (f *Field) ErrorClass() string {
-	if f.Error != nil {
-		return ERROR_CLASS
-	}
-	return ""
-}
-
-// Return "checked" if this field.Value matches the provided value
-func (f *Field) Checked(val string) string {
-	if f.Value == val {
-		return "checked"
-	}
-	return ""
-}
-
 var (
 	// The functions available for use in the templates.
 	TemplateFuncs = map[string]interface{}{
@@ -71,18 +51,10 @@ var (
 			}
 			return template.HTML("")
 		},
-		"field": func(name string, renderArgs map[string]interface{}) *Field {
-			value, _ := renderArgs["flash"].(map[string]string)[name]
-			err, _ := renderArgs["errors"].(map[string]*ValidationError)[name]
-			return &Field{
-				Name:  name,
-				Value: value,
-				Error: err,
-			}
-		},
+		"field": NewField,
 		"option": func(f *Field, val, label string) template.HTML {
 			selected := ""
-			if f.Value == val {
+			if f.Flash() == val {
 				selected = " selected"
 			}
 			return template.HTML(fmt.Sprintf(`<option value="%s"%s>%s</option>`,
@@ -90,7 +62,7 @@ var (
 		},
 		"radio": func(f *Field, val string) template.HTML {
 			checked := ""
-			if f.Value == val {
+			if f.Flash() == val {
 				checked = " checked"
 			}
 			return template.HTML(fmt.Sprintf(`<input type="radio" name="%s" value="%s"%s>`,
