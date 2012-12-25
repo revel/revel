06fe918eecbd3be3547995aab155bdb760db78e3
tmbrggmn <tmbrggmn@gmail.com>
2012-12-25 21:38:03 +0100

    Added i18n plugin tests

---
+++ i18n_plugin_test.go
@@ -0,0 +1,99 @@
+package rev
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+	"time"
+)
+
+const (
+	testConfigPath string = "testdata/i18n/config"
+	testConfigName string = "test_app.conf"
+)
+
+func TestHasLocaleCookie(t *testing.T) {
+	stubConfig(t)
+
+	if found, value := hasLocaleCookie(buildRequestWithCookie("REVEL_LANG", "en")); !found {
+		t.Errorf("Expected %s cookie with value '%s' but found nothing or unexpected value '%s'", "REVEL_LANG", "en", value)
+	}
+	if found, value := hasLocaleCookie(buildRequestWithCookie("REVEL_LANG", "en-US")); !found {
+		t.Errorf("Expected %s cookie with value '%s' but found nothing or unexpected value '%s'", "REVEL_LANG", "en-US", value)
+	}
+	if found, _ := hasLocaleCookie(buildRequestWithCookie("DOESNT_EXIST", "en-US")); found {
+		t.Errorf("Expected %s cookie to not exist, but apparently it does", "DOESNT_EXIST")
+	}
+}
+
+func TestHasLocaleCookieWithInvalidConfig(t *testing.T) {
+	stubConfigWithoutLanguageCookieOption(t)
+	if found, _ := hasLocaleCookie(buildRequestWithCookie("REVEL_LANG", "en-US")); found {
+		t.Errorf("Expected %s cookie to not exist because the configured name is missing", "REVEL_LANG")
+	}
+}
+
+func TestHasAcceptLanguageHeader(t *testing.T) {
+	if found, value := hasAcceptLanguageHeader(buildRequestWithAcceptLanguages("en-US")); !found && value != "en-US" {
+		t.Errorf("Expected to find Accept-Language header with value '%s', found '%s' instead", "en-US", value)
+	}
+	if found, value := hasAcceptLanguageHeader(buildRequestWithAcceptLanguages("en-GB", "en-US", "nl")); !found && value != "en-GB" {
+		t.Errorf("Expected to find Accept-Language header with value '%s', found '%s' instead", "en-GB", value)
+	}
+}
+
+func TestBeforeRequest(t *testing.T) {
+	stubConfig(t)
+	plugin := I18nPlugin{}
+
+	controller := NewController(buildEmptyRequest(), nil, &ControllerType{reflect.TypeOf(Controller{}), nil})
+	if plugin.BeforeRequest(controller); controller.Args[CurrentLocaleControllerArg] != "" {
+		t.Errorf("Expected to find current language '%s' in controller, found '%s' instead", "", controller.Args[CurrentLocaleControllerArg])
+	}
+
+	controller = NewController(buildRequestWithCookie("REVEL_LANG", "en-US"), nil, &ControllerType{reflect.TypeOf(Controller{}), nil})
+	if plugin.BeforeRequest(controller); controller.Args[CurrentLocaleControllerArg] != "en-US" {
+		t.Errorf("Expected to find current language '%s' in controller, found '%s' instead", "en-US", controller.Args[CurrentLocaleControllerArg])
+	}
+
+	controller = NewController(buildRequestWithAcceptLanguages("en-GB", "en-US"), nil, &ControllerType{reflect.TypeOf(Controller{}), nil})
+	if plugin.BeforeRequest(controller); controller.Args[CurrentLocaleControllerArg] != "en-GB" {
+		t.Errorf("Expected to find current language '%s' in controller, found '%s' instead", "en-GB", controller.Args[CurrentLocaleControllerArg])
+	}
+}
+
+func stubConfig(t *testing.T) {
+	ConfPaths = append(ConfPaths, testConfigPath)
+	testConfig, error := LoadConfig(testConfigName)
+	if error != nil {
+		t.Fatalf("Unable to load test config '%s': %s", testConfigName, error.Error())
+	}
+	Config = testConfig
+}
+
+func stubConfigWithoutLanguageCookieOption(t *testing.T) {
+	stubConfig(t)
+	Config.config.RemoveOption("DEFAULT", "i18n.cookie")
+}
+
+func buildRequestWithCookie(name, value string) *Request {
+	httpRequest, _ := http.NewRequest("GET", "/", nil)
+	request := NewRequest(httpRequest)
+	request.AddCookie(&http.Cookie{name, value, "", "", time.Now(), "", 0, false, false, "", nil})
+	return request
+}
+
+func buildRequestWithAcceptLanguages(acceptLanguages ...string) *Request {
+	httpRequest, _ := http.NewRequest("GET", "/", nil)
+	request := NewRequest(httpRequest)
+	for _, acceptLanguage := range acceptLanguages {
+		request.AcceptLanguages = append(request.AcceptLanguages, AcceptLanguage{acceptLanguage, 1})
+	}
+	return request
+}
+
+func buildEmptyRequest() *Request {
+	httpRequest, _ := http.NewRequest("GET", "/", nil)
+	request := NewRequest(httpRequest)
+	return request
+}
