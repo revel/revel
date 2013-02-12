2d8c299060a4fb0fdd5c968d6c69b8e48a6e974d
Rob Figueiredo <robfig@gmail.com>
2013-02-11 22:22:14 -0500

    Retry commit: "Clean up pluralize function, add some docs"

--- template.go
+++ template.go
@@ -106,10 +106,12 @@ var (
 			return template.HTML(text)
 		},
 
-		// Pluralize
-		"pluralize": func(item interface{}, pluralOverrides ...string) string {
+		// Pluralize, a helper for pluralizing words to correspond to data of dynamic length.
+		// items - a slice of items, or an integer indicating how many items there are.
+		// pluralOverrides - optional arguments specifying the output in the
+		//     singular and plural cases.  by default "" and "s"
+		"pluralize": func(items interface{}, pluralOverrides ...string) string {
 			singular, plural := "", "s"
-
 			if len(pluralOverrides) >= 1 {
 				singular = pluralOverrides[0]
 				if len(pluralOverrides) == 2 {
@@ -117,10 +119,9 @@ var (
 				}
 			}
 
-			switch v := reflect.ValueOf(item); v.Kind() {
-
+			switch v := reflect.ValueOf(items); v.Kind() {
 			case reflect.Int:
-				if item.(int) != 1 {
+				if items.(int) != 1 {
 					return plural
 				}
 			case reflect.Slice:
@@ -130,9 +131,7 @@ var (
 			default:
 				ERROR.Println("pluralize: unexpected type: ", v)
 			}
-
 			return singular
-
 		},
 
 		// Format a date according to the application's default date(time) format.
