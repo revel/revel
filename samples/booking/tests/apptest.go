c88d46cf5c3340e1f02f32f218ef183acbae3eb6
Rob Figueiredo <robfig@gmail.com>
2013-02-20 22:13:25 -0500

    Update the example test to use pointer receivers.

--- samples/booking/tests/apptest.go
+++ samples/booking/tests/apptest.go
@@ -6,16 +6,16 @@ type ApplicationTest struct {
 	revel.TestSuite
 }
 
-func (t ApplicationTest) Before() {
+func (t *ApplicationTest) Before() {
 	println("Set up")
 }
 
-func (t ApplicationTest) TestThatIndexPageWorks() {
+func (t *ApplicationTest) TestThatIndexPageWorks() {
 	t.Get("/")
 	t.AssertOk()
 	t.AssertContentType("text/html")
 }
 
-func (t ApplicationTest) After() {
+func (t *ApplicationTest) After() {
 	println("Tear down")
 }
