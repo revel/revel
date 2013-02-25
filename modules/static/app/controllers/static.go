ae0eda89e3ec42dda546dc672c05cfa182002d73
Jeff Graham <jeff@funnymonkey.com>
2013-02-25 11:10:11 -0800

    Adding documentation for Static.Serve and Static.ServeModule

--- modules/static/app/controllers/static.go
+++ modules/static/app/controllers/static.go
@@ -10,6 +10,36 @@ type Static struct {
 	*revel.Controller
 }
 
+// This method handles requests for files. The supplied prefix may be absolute
+// or relative. If the prefix is relative it is assumed to be relative to the
+// application directory. The filepath may either be just a file or an
+// additional filepath to search for the given file. This response may return
+// the following responses in the event of an error or invalid request;
+//   403(Forbidden): If the prefix filepath combination results in a directory.
+//   404(Not found): If the prefix and filepath combination results in a non-existent file.
+//   500(Internal Server Error): There are a few edge cases that would likely indicate some configuration error outside of revel.
+//
+// Note that when defining routes in routes/conf the parameters must not have
+// spaces around the comma.
+//   Bad:  Static.Serve("public/img", "favicon.png")
+//   Good: Static.Serve("public/img","favicon.png")
+//
+// Examples:
+// Serving a directory
+//   Route (conf/routes):
+//     GET /public/{<.*>filepath} Static.Serve("public")
+//   Request:
+//     public/js/sessvars.js
+//   Calls
+//     Static.Serve("public","js/sessvars.js")
+//
+// Serving a file
+//   Route (conf/routes):
+//     GET /favicon.ico Static.Serve("public/img","favicon.png")
+//   Request:
+//     favicon.ico
+//   Calls:
+//     Static.Serve("public/img", "favicon.png")
 func (c Static) Serve(prefix, filepath string) revel.Result {
 	var basePath string
 
@@ -43,6 +73,9 @@ func (c Static) Serve(prefix, filepath string) revel.Result {
 
 }
 
+// This method allows modules to serve binary files. The parameters are the same
+// as Static.Serve with the additional module name pre-pended to the list of
+// arguments.
 func (c Static) ServeModule(moduleName, prefix, filepath string) revel.Result {
 	var basePath string
 	for _, module := range revel.Modules {
