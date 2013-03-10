dedff5a385ccec737da41c0c8491b75bcf6332a3
Rob Figueiredo <robfig@gmail.com>
2013-03-10 19:56:23 -0400

    Static: Return 404 instead of 500 for file-not-found

--- modules/static/app/controllers/static.go
+++ modules/static/app/controllers/static.go
@@ -56,27 +56,22 @@ func (c Static) Serve(prefix, filepath string) revel.Result {
 	}
 
 	finfo, err := os.Stat(fname)
-
-	if err == nil {
-		if finfo.Mode().IsDir() {
-			revel.WARN.Printf("Attempted directory listing of %s", fname)
-			return c.Forbidden("Directory listing not allowed")
-		}
-		file, err := os.Open(fname)
+	if err != nil {
 		if os.IsNotExist(err) {
 			revel.WARN.Printf("File not found (%s): %s ", fname, err)
 			return c.NotFound("File not found")
-		} else if err != nil {
-			revel.WARN.Printf("Problem opening file (%s): %s ", fname, err)
-			return c.RenderError(err)
 		}
-		return c.RenderFile(file, "")
-	} else {
 		revel.ERROR.Printf("Error trying to get fileinfo for '%s': %s", fname, err)
+		return c.RenderError(err)
 	}
 
-	return c.RenderError(err)
+	if finfo.Mode().IsDir() {
+		revel.WARN.Printf("Attempted directory listing of %s", fname)
+		return c.Forbidden("Directory listing not allowed")
+	}
 
+	file, err := os.Open(fname)
+	return c.RenderFile(file, "")
 }
 
 // This method allows modules to serve binary files. The parameters are the same
