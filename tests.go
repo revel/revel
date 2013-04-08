f048c4f0c4502d95045f50b2313f429216001361
Nolan Evans <nolane@gmail.com>
2013-04-08 08:50:34 -0700

    adding a test method to create a websocket connection

--- tests.go
+++ tests.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"code.google.com/p/go.net/websocket"
 )
 
 type TestSuite struct {
@@ -23,14 +24,23 @@ func NewTestSuite() TestSuite {
 	return TestSuite{Client: &http.Client{}}
 }
 
-// Return the base URL of the server, e.g. "http://127.0.0.1:8557"
-func (t *TestSuite) BaseUrl() string {
+// Return the address and port of the server, e.g. "127.0.0.1:8557"
+func (t *TestSuite) AddrAndPort() string {
 	if Server.Addr[0] == ':' {
-		return "http://127.0.0.1" + Server.Addr
+		return "127.0.0.1" + Server.Addr
 	}
-	return "http://" + Server.Addr
+	return Server.Addr
 }
 
+// Return the base http URL of the server, e.g. "http://127.0.0.1:8557"
+func (t *TestSuite) BaseUrl() string {
+	return "http://" + t.AddrAndPort()
+}
+
+// Return the base websocket URL of the server, e.g. "ws://127.0.0.1:8557"
+func (t *TestSuite) WebSocketUrl() string {
+	return "ws://" + t.AddrAndPort()
+}
 // Issue a GET request to the given path and store the result in Request and
 // RequestBody.
 func (t *TestSuite) Get(path string) {
@@ -68,6 +78,17 @@ func (t *TestSuite) MakeRequest(req *http.Request) {
 	if t.ResponseBody, err = ioutil.ReadAll(t.Response.Body); err != nil {
 		panic(err)
 	}
+}
+
+// Create a websocket connection to the given path and return the connection
+func (t *TestSuite) WebSocket(path string) *websocket.Conn{
+	origin := t.BaseUrl() + "/"
+	url := t.WebSocketUrl() + path
+	ws, err := websocket.Dial(url, "", origin)
+	if err != nil {
+		panic(err)
+	}
+	return ws
 }
 
 func (t *TestSuite) AssertOk() {
