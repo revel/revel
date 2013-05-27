5fdf98d9377a4abde5959e42ba7094f232405d43
Rob Figueiredo <robfig@gmail.com>
2013-05-27 18:15:38 -0400

    Comment the default filter chain

--- filter.go
+++ filter.go
@@ -5,16 +5,16 @@ type Filter func(c *Controller, filterChain []Filter)
 // Filters is the default set of global filters.
 // It may be set by the application on initialization.
 var Filters = []Filter{
-	PanicFilter,
-	RouterFilter,
-	FilterConfiguringFilter,
-	ParamsFilter,
-	SessionFilter,
-	FlashFilter,
-	ValidationFilter,
-	I18nFilter,
-	InterceptorFilter,
-	ActionInvoker,
+	PanicFilter,             // Recover from panics and display an error page instead.
+	RouterFilter,            // Use the routing table to select the right Action
+	FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
+	ParamsFilter,            // Parse parameters into Controller.Params.
+	SessionFilter,           // Restore and write the session cookie.
+	FlashFilter,             // Restore and write the flash cookie.
+	ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
+	I18nFilter,              // Resolve the requested language
+	InterceptorFilter,       // Run interceptors around the action.
+	ActionInvoker,           // Invoke the action.
 }
 
 // NilFilter and NilChain are helpful in writing filter tests.
--- skeleton/app/init.go
+++ skeleton/app/init.go
@@ -3,16 +3,17 @@ package app
 import "github.com/robfig/revel"
 
 func init() {
-	revel.Filters = []revel.Filter{
-		revel.PanicFilter,
-		revel.RouterFilter,
-		revel.FilterConfiguringFilter,
-		revel.ParamsFilter,
-		revel.SessionFilter,
-		revel.FlashFilter,
-		revel.ValidationFilter,
-		revel.I18nFilter,
-		revel.InterceptorFilter,
-		revel.ActionInvoker,
+	// Filters is the default set of global filters.
+	revel.Filters = []Filter{
+		PanicFilter,             // Recover from panics and display an error page instead.
+		RouterFilter,            // Use the routing table to select the right Action
+		FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
+		ParamsFilter,            // Parse parameters into Controller.Params.
+		SessionFilter,           // Restore and write the session cookie.
+		FlashFilter,             // Restore and write the flash cookie.
+		ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
+		I18nFilter,              // Resolve the requested language
+		InterceptorFilter,       // Run interceptors around the action.
+		ActionInvoker,           // Invoke the action.
 	}
 }
