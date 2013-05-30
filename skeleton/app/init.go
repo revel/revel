f525f34f177a9192e2e62c9dd3ff7e29944b0817
Rob Figueiredo <robfig@gmail.com>
2013-05-29 22:14:44 -0400

    Fix: Add import path to app/init.go filters

--- skeleton/app/init.go
+++ skeleton/app/init.go
@@ -4,16 +4,16 @@ import "github.com/robfig/revel"
 
 func init() {
 	// Filters is the default set of global filters.
-	revel.Filters = []Filter{
-		PanicFilter,             // Recover from panics and display an error page instead.
-		RouterFilter,            // Use the routing table to select the right Action
-		FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
-		ParamsFilter,            // Parse parameters into Controller.Params.
-		SessionFilter,           // Restore and write the session cookie.
-		FlashFilter,             // Restore and write the flash cookie.
-		ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
-		I18nFilter,              // Resolve the requested language
-		InterceptorFilter,       // Run interceptors around the action.
-		ActionInvoker,           // Invoke the action.
+	revel.Filters = []revel.Filter{
+		revel.PanicFilter,             // Recover from panics and display an error page instead.
+		revel.RouterFilter,            // Use the routing table to select the right Action
+		revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
+		revel.ParamsFilter,            // Parse parameters into Controller.Params.
+		revel.SessionFilter,           // Restore and write the session cookie.
+		revel.FlashFilter,             // Restore and write the flash cookie.
+		revel.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
+		revel.I18nFilter,              // Resolve the requested language
+		revel.InterceptorFilter,       // Run interceptors around the action.
+		revel.ActionInvoker,           // Invoke the action.
 	}
 }
