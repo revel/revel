94317fe784468b51c9099173997b7219ce447df1
Adam Frisby <adamfrisby@gmail.com>
2013-06-11 16:35:28 -0400

    Use c.Name instead of c.Type.Type.Name() a second time

--- controller.go
+++ controller.go
@@ -223,7 +223,7 @@ func (c *Controller) SetAction(controllerName, methodName string) error {
 	}
 
 	c.Name, c.MethodName = c.Type.Type.Name(), methodName
-	c.Action = c.Type.Type.Name() + "." + c.MethodName
+	c.Action = c.Name + "." + c.MethodName
 
 	// Instantiate the controller.
 	c.AppController = initNewAppController(c.Type.Type, c).Interface()
