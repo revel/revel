34d8c95b07177943689ab8bd77f0da7278513ef1
John C. Vernaleo <john@netpurgatory.com>
2013-07-31 17:49:05 -0400

    Modify RenderFile to call RenderBinary.

--- controller.go
+++ controller.go
@@ -179,23 +179,14 @@ func (c *Controller) RenderFile(file *os.File, delivery ContentDisposition) Resu
 	if fileInfo != nil {
 		modtime = fileInfo.ModTime()
 	}
-	return &BinaryResult{
-		Reader:   file,
-		Name:     filepath.Base(file.Name()),
-		Delivery: delivery,
-		Length:   -1, // http.ServeContent gets the length itself
-		ModTime:  modtime,
-	}
+	return c.RenderBinary(file, filepath.Base(file.Name()), delivery, modtime)
 }
 
 // RenderBinary is like RenderFile() except that it instead of a file on disk,
 // it renders data from memory (which could be a file that has not been written,
 // the output from some function, or bytes streamed from somewhere else, as long
 // it implements io.Reader).
-func (c *Controller) RenderBinary(memfile io.Reader, filename string, delivery ContentDisposition) Result {
-	var (
-		modtime = time.Now()
-	)
+func (c *Controller) RenderBinary(memfile io.Reader, filename string, delivery ContentDisposition, modtime time.Time) Result {
 	return &BinaryResult{
 		Reader:   memfile,
 		Name:     filename,
