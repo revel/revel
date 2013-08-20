c057faae146b843cfd1b54dd9c44735cc4932bc6
Shane Logsdon <shane.a.logsdon@gmail.com>
2013-08-19 23:36:13 -0400

    Use method for passing types. documentation

--- modules/auth/app/plugin.go
+++ modules/auth/app/plugin.go
@@ -12,18 +12,6 @@ const (
 	DEFAULT_USERNAME_FIELD = "Username"
 )
 
-// struct for passing user-defined structs for use in authentication
-type AuthStructs struct {
-	Session *interface{}
-	User    *interface{}
-}
-
-// defines resource that needs authentication
-type AuthenticatedResource struct {
-	Resource interface{}
-	Role     string // TODO: implement role-based ACL config
-}
-
 var (
 	Structs       AuthStructs
 	UseRoles      bool
@@ -32,10 +20,12 @@ var (
 )
 
 func init() {
-	PasswordField = revel.Config.
-		StringDefault("auth.passwordfield", DEFAULT_PASSWORD_FIELD)
-	UsernameField = revel.Config.
-		StringDefault("auth.usernamefield", DEFAULT_USERNAME_FIELD)
+	revel.OnAppStart(func() {
+		PasswordField = revel.Config.
+			StringDefault("auth.passwordfield", DEFAULT_PASSWORD_FIELD)
+		UsernameField = revel.Config.
+			StringDefault("auth.usernamefield", DEFAULT_USERNAME_FIELD)
+	})
 }
 
 // The actual filter added to the resource. It checks for valid session
@@ -45,7 +35,7 @@ var SessionAuthenticationFilter = func(c *revel.Controller, fc []revel.Filter) {
 	if Structs.User == nil {
 		revel.ERROR.Fatal("User struct has not been passed.")
 	}
-	if !false { // TODO: check for session information
+	if valid := CheckSession(); !valid {
 		c.Flash.Error("Session invalid. Please login.")
 		c.Response.Status = http.StatusFound
 		c.Response.Out.Header().Add("Location", "/session/create")
@@ -54,7 +44,9 @@ var SessionAuthenticationFilter = func(c *revel.Controller, fc []revel.Filter) {
 	fc[0](c, fc[1:]) // Execute the next filter stage.
 }
 
-// auth.Apply is run by the developer in the init.go file for his/her project.
+func CheckSession
+
+// Apply is run by the developer in the init.go file for his/her project.
 // It loops over the slice for all AuthenticatedResources the developer wishes
 // to be protected with authentication.
 func Apply(m []AuthenticatedResource) {
@@ -69,14 +61,48 @@ func Apply(m []AuthenticatedResource) {
 	}
 }
 
+// Use is run by the developer in the init.go file for his/her project.
+// It should contain references the the structs used to contain user and,
+// optionally, session information. It also verifies the structs contain the
+// expected fields, whether the defaults or those defined in the app.conf.
+//
+// Example:
+//     import (
+//         auth "github.com/robfig/revel/auth/app"
+//         "project/models"
+//     )
+//
+//     func init() {
+//         revel.OnAppStart(func() {
+//	           auth.Use(auth.AuthStructs{
+//	               Session: models.Session{},
+//                 User:    models.User{},
+//             })
+//         })
+//     }
 func Use(s AuthStructs) {
-	// test for expected fields
 	var found bool
 	if _, found = reflect.TypeOf(s.User).FieldByName(PasswordField); !found {
-		revel.ERROR.Fatal(fmt.Sprintf("Expecting a User struct that contains the field '%v'.", PasswordField))
+		revel.ERROR.Fatal(fmt.Sprintf(
+			"Expecting a User struct that contains the field '%v'.",
+			PasswordField))
 	}
 	if _, found = reflect.TypeOf(s.User).FieldByName(UsernameField); !found {
-		revel.ERROR.Fatal(fmt.Sprintf("Expecting a User struct that contains the field '%v'.", UsernameField))
+		revel.ERROR.Fatal(fmt.Sprintf(
+			"Expecting a User struct that contains the field '%v'.",
+			UsernameField))
 	}
 	Structs = s
+}
+
+// struct for passing user-defined structs for use in authentication
+type AuthStructs struct {
+	Session interface{}
+	User    interface{}
+}
+
+// defines resource that needs authentication
+type AuthenticatedResource struct {
+	Resource interface{}
+	Role     string // TODO: allow role-based ACL config
 }
