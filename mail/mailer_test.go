d030a845e255a53bbbbb3b5da8bb6910942ef370
arkxu <fangzhou@arkxu.com>
2013-09-09 00:42:17 -0700

    add default sender capability, tune the APIs little bit

--- mail/mailer_test.go
+++ mail/mailer_test.go
@@ -6,25 +6,32 @@ import (
 )
 
 func TestSend(t *testing.T) {
-	mailer := &Mailer{Address: "smtp.gmail.com", Port: 587, UserName: "fangzhou@arkxu.com", Password: "xxxxxxx", Authentication: "plain"}
+	mailer := &Mailer{Address: "smtp.gmail.com", Port: 587, UserName: "fangzhou@arkxu.com", Password: "xxxxx"}
 
 	message1 := &Message{From: "fangzhou@arkxu.com", To: []string{"fangzhou@arkxu.com"},
-		Subject: "这个是第1封from message1, single connection", PlainBody: "<h2>你好 from message1, should show in plain text</h2>"}
+		Subject: "这个是第11封from message1, single connection", PlainBody: "<h2>你好 from message1, should show in plain text</h2>"}
 
 	message2 := &Message{From: "fangzhou@arkxu.com", To: []string{"fangzhou@arkxu.com"},
-		Subject: "这个是第2封from message2, single connection", HtmlBody: "<h2>您好 from message2</h2>"}
+		Subject: "这个是第12封from message2, single connection", HtmlBody: "<h2>您好 from message2</h2>"}
 
 	err := mailer.SendMails([]*Message{message1, message2})
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	err = mailer.SendMail("fangzhou@arkxu.com", []string{"fangzhou@arkxu.com"}, "我的第3个", "这个不是html的", false)
+}
+
+func TestWithDefaultSender(t *testing.T) {
+	mailer := &Mailer{Address: "smtp.gmail.com", Port: 587, UserName: "fangzhou@arkxu.com", Password: "xxxxx"}
+
+	mailer.DefaultSender = &DefaultSender{From: "fangzhou@arkxu.com"}
+
+	err := mailer.SendMail([]string{"fangzhou@arkxu.com"}, "我的第13个", "这个不是html的", false)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	err = mailer.SendMail("fangzhou@arkxu.com", []string{"fangzhou@arkxu.com"}, "我的第4个", "<h1>这个是html的</h1>", true)
+	err = mailer.SendMail([]string{"fangzhou@arkxu.com"}, "我的第14个", "<h1>这个是html的</h1>", true)
 	if err != nil {
 		fmt.Println(err)
 	}
