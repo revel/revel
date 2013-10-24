715800f600b5999d7af86353020f8f82e526b4b5
verdverm <verdverm@gmail.com>
2013-10-23 21:19:46 -0400

    moved rand to rev.go, help msg change, made getSkeletonBase(cmd_ln_args) to determine skeleton import path

--- revel/new.go
+++ revel/new.go
@@ -4,11 +4,9 @@ import (
 	"bytes"
 	"fmt"
 	"go/build"
-	"math/rand"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"time"
 
 	"github.com/golang/glog"
 	"github.com/robfig/revel"
@@ -23,7 +21,7 @@ New creates a few files to get a new Revel application running quickly.
 It puts all of the files in the given import path, taking the final element in
 the path to be the app name.
 
-Skeleton is an optional argument, and should be relative to $GOROOT.
+Skeleton is an optional argument, provided as an import path
 
 For example:
 
@@ -34,7 +32,6 @@ For example:
 }
 
 func init() {
-	rand.Seed(time.Now().Unix())
 	cmdNew.Run = newApp
 }
 
@@ -82,30 +79,8 @@ func newApp(args []string) {
 	err = os.MkdirAll(appDir, 0777)
 	panicOnError(err, "Failed to create directory "+appDir)
 
-	// specifying skeleton
-	if len(args) == 2 { // user specified
-		sname := args[1]
-		_, errS := build.Import(sname, "", build.FindOnly)
-		if errS != nil {
-			fmt.Println("go get'n: ", args[1])
-
-			// Execute "go get <pkg>"
-			getCmd := exec.Command(goExec, "get", "-d", sname)
-			glog.V(1).Infoln("Exec:", getCmd.Args)
-			getOutput, errG := getCmd.CombinedOutput()
-
-			// check getOutput for no buildible string
-			bpos := bytes.Index(getOutput, []byte("no buildable Go source files in"))
-			if errG != nil && bpos == -1 {
-				fmt.Fprintf(os.Stderr, "Abort: Could not find or 'go get' Skeleton  source code: %s\n%s\n", getOutput, sname)
-				return
-			}
-		}
-		skeletonBase = filepath.Join(srcRoot, sname)
-
-	} else { // use the revel default (bootstrap)
-		skeletonBase = filepath.Join(revelPkg.Dir, "skeleton")
-	}
+	// checking and setting skeleton
+	skeletonBase = getSkeletonBase(args)
 
 	mustCopyDir(appDir, skeletonBase, map[string]interface{}{
 		// app.conf
@@ -129,4 +104,33 @@ func genSecret() string {
 		chars[i] = alphaNumeric[rand.Intn(len(alphaNumeric))]
 	}
 	return string(chars)
+}
+
+// getSkeletonBase determines the correct skeleton
+// base from the command line arguments.
+// If none is specified, then the revel default is used.
+// in:   command line arguments to revel
+// out:  the source directory for the skeleton
+func getSkeletonBase(args []string) string {
+	if len(args) == 2 { // user specified
+		skeleton_name := args[1]
+		_, errS := build.Import(skeleton_name, "", build.FindOnly)
+		if errS != nil {
+			// Execute "go get <pkg>"
+			getCmd := exec.Command(goExec, "get", "-d", skeleton_name)
+			glog.V(1).Infoln("Exec:", getCmd.Args)
+			getOutput, errG := getCmd.CombinedOutput()
+
+			// check getOutput for no buildible string
+			bpos := bytes.Index(getOutput, []byte("no buildable Go source files in"))
+			if errG != nil && bpos == -1 {
+				fmt.Fprintf(os.Stderr, "Abort: Could not find or 'go get' Skeleton  source code: %s\n%s\n", getOutput, skeleton_name)
+				return
+			}
+		}
+		return filepath.Join(srcRoot, skeleton_name)
+
+	} else { // use the revel default (bootstrap)
+		return filepath.Join(revelPkg.Dir, "skeleton")
+	}
 }
