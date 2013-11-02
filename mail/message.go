7d471a2a88a004cbe06636f51fb2330b91ce9ee8
arkxu <fangzhou@arkxu.com>
2013-11-02 05:52:23 -0700

    use Buffer as internal body, some api changes too

--- mail/message.go
+++ mail/message.go
@@ -6,78 +6,202 @@ import (
 	"fmt"
 	"mime/multipart"
 	"strings"
+
+	"github.com/robfig/revel"
 )
+
+var NewLine string = "\r\n"
 
 type Message struct {
 	From      string
-	ReplyTo   string //implement later
+	ReplyTo   string
 	To        []string
-	BCC       string //implement later
-	CC        string //implement later
+	Cc        []string
+	Bcc       []string
 	Subject   string
-	PlainBody string
-	HtmlBody  string
+	PlainBody *bytes.Buffer
+	HtmlBody  *bytes.Buffer
+}
+
+// NewTextMessage create a plain text message
+func NewTextMessage(to []string, subject string, body string) *Message {
+	return &Message{To: to, Subject: subject, PlainBody: bytes.NewBufferString(body)}
+}
+
+// NewHtmlMessage create a html message
+func NewHtmlMessage(to []string, subject string, body string) *Message {
+	return &Message{To: to, Subject: subject, HtmlBody: bytes.NewBufferString(body)}
+}
+
+// NewTextAndHtmlMessage create a message contains both plain text and html message
+func NewTextAndHtmlMessage(to []string, subject string, plainBody string, htmlBody string) *Message {
+	return &Message{To: to, Subject: subject, PlainBody: bytes.NewBufferString(plainBody), HtmlBody: bytes.NewBufferString(htmlBody)}
 }
 
 // RenderData render the whole email body
-func (m *Message) RenderData() (data string, err error) {
-	if m.HtmlBody == "" && m.PlainBody == "" {
+func (m *Message) RenderData() (data []byte, err error) {
+	if m.HtmlBody == nil && m.PlainBody == nil {
 		err = errors.New("HtmlBody and PlainBody can not both be blank")
 		return
 	}
-	if m.HtmlBody == "" {
-		data = m.renderSingleContentType("text/plain", m.PlainBody)
-	} else if m.PlainBody == "" {
-		data = m.renderSingleContentType("text/html", m.HtmlBody)
-	} else {
-		data = m.renderPlainAndHtmlText()
+
+	switch {
+	case m.HtmlBody == nil, m.HtmlBody.Len() == 0:
+		data, err = m.renderSingleContentType("text/plain", m.PlainBody)
+	case m.PlainBody == nil, m.PlainBody.Len() == 0:
+		data, err = m.renderSingleContentType("text/html", m.HtmlBody)
+	default:
+		data, err = m.renderPlainAndHtmlText()
 	}
 
 	return
 }
 
-func (m *Message) renderSingleContentType(contentType string, bodyText string) string {
-	body :=
-		`From: %s
-To: %s
-Subject: %s
-MIME-Version: 1.0
+// RenderTemplate renders the message body from the template and input parameters, the change is inline the message
+func (m *Message) RenderTemplate(templatePath string, args map[string]interface{}) error {
+	m.HtmlBody = m.renderViewTemplate(templatePath+".html", args)
+	m.PlainBody = m.renderViewTemplate(templatePath+".txt", args)
+
+	if m.HtmlBody == nil && m.PlainBody == nil {
+		return errors.New("Both HTML body and Plain body are blank.")
+	}
+	return nil
+}
+
+func (m *Message) renderViewTemplate(templateFilePath string, args map[string]interface{}) *bytes.Buffer {
+	// Get the Template.
+	template, err := revel.MainTemplateLoader.Template(templateFilePath)
+	if err != nil {
+		return nil
+	}
+
+	var b bytes.Buffer
+
+	err = template.Render(&b, args)
+	if err != nil {
+		return nil
+	}
+
+	return &b
+}
+
+func (m *Message) renderSingleContentType(contentType string, bodyText *bytes.Buffer) ([]byte, error) {
+	var b bytes.Buffer
+	m.writeRecipient(&b)
+
+	if err := m.writeMIME(&b); err != nil {
+		return nil, err
+	}
+
+	if err := m.writeContentType(&b, contentType); err != nil {
+		return nil, err
+	}
+
+	if _, err := b.Write(bodyText.Bytes()); err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+func (m *Message) renderPlainAndHtmlText() ([]byte, error) {
+	writer := multipart.NewWriter(bytes.NewBufferString(""))
+	var b bytes.Buffer
+	m.writeRecipient(&b)
+
+	if err := m.writeMIME(&b); err != nil {
+		return nil, err
+	}
+
+	if err := m.writeMultipartStart(&b, writer); err != nil {
+		return nil, err
+	}
+
+	if err := m.writeContentTypeWithBoundary(&b, writer, "text/plain"); err != nil {
+		return nil, err
+	}
+
+	if _, err := b.Write(m.PlainBody.Bytes()); err != nil {
+		return nil, err
+	}
+
+	if err := m.writeContentTypeWithBoundary(&b, writer, "text/html"); err != nil {
+		return nil, err
+	}
+
+	if _, err := b.Write(m.HtmlBody.Bytes()); err != nil {
+		return nil, err
+	}
+
+	if err := m.writeMultipartEnd(&b, writer); err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+func (m *Message) writeRecipient(b *bytes.Buffer) {
+	if m.From != "" {
+		fmt.Fprintf(b, "From: %s %s", m.From, NewLine)
+	}
+
+	if m.ReplyTo != "" {
+		fmt.Fprintf(b, "Reply-To: %s %s", m.ReplyTo, NewLine)
+	}
+
+	if len(m.To) > 0 {
+		fmt.Fprintf(b, "To: %s %s", strings.Join(m.To, ", "), NewLine)
+	}
+
+	if len(m.Cc) > 0 {
+		fmt.Fprintf(b, "Cc: %s %s", strings.Join(m.Cc, ", "), NewLine)
+	}
+
+	if m.Subject != "" {
+		fmt.Fprintf(b, "Subject: %s %s", m.Subject, NewLine)
+	}
+}
+
+func (m *Message) writeMIME(b *bytes.Buffer) error {
+	_, err := b.WriteString("MIME-Version: 1.0")
+	return err
+}
+
+func (m *Message) writeContentType(b *bytes.Buffer, contentType string) error {
+	contentTypeFormat := `
 Content-Type: %s; charset="UTF-8";
 Content-Transfer-Encoding: 8bit
 
-%s
-
 `
-
-	data := fmt.Sprintf(body, m.From, strings.Join(m.To, ","), m.Subject, contentType, bodyText)
-	return data
+	_, err := b.WriteString(fmt.Sprintf(contentTypeFormat, contentType))
+	return err
 }
 
-func (m *Message) renderPlainAndHtmlText() string {
-	writer := multipart.NewWriter(bytes.NewBufferString(""))
-	body :=
-		`From: %s
-To: %s
-Subject: %s
-MIME-Version: 1.0
+func (m *Message) writeMultipartStart(b *bytes.Buffer, writer *multipart.Writer) error {
+	multipart := `
 Content-Type: multipart/alternative; charset="UTF-8"; boundary="%s"
+`
+	_, err := b.WriteString(fmt.Sprintf(multipart, writer.Boundary()))
+	return err
+}
 
---%s
-Content-Type: text/plain; charset=UTF-8
-Content-Transfer-Encoding: 8bit
-
-%s
-
---%s
-Content-Type: text/html; charset=UTF-8
-Content-Transfer-Encoding: 8bit
-
-%s
-
+func (m *Message) writeMultipartEnd(b *bytes.Buffer, writer *multipart.Writer) error {
+	multipart := `
 --%s--
 
 `
-	data := fmt.Sprintf(body, m.From, strings.Join(m.To, ","), m.Subject, writer.Boundary(),
-		writer.Boundary(), m.PlainBody, writer.Boundary(), m.HtmlBody, writer.Boundary())
-	return data
+	_, err := b.WriteString(fmt.Sprintf(multipart, writer.Boundary()))
+	return err
+}
+
+func (m *Message) writeContentTypeWithBoundary(b *bytes.Buffer, writer *multipart.Writer, contentType string) error {
+	contentTypeFormat := `
+
+--%s
+Content-Type: %s; charset=UTF-8;
+Content-Transfer-Encoding: 8bit
+
+`
+	_, err := b.WriteString(fmt.Sprintf(contentTypeFormat, writer.Boundary(), contentType))
+	return err
 }
