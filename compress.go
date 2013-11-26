75db52be71b19dcd648ab3f2f405d4f5ec830e6e
Robert Wertman <robert.wertman@gmail.com>
2013-11-25 22:26:27 -0500

    Added q=0.X detection.

--- compress.go
+++ compress.go
@@ -4,6 +4,7 @@ import (
 	"compress/gzip"
 	"compress/zlib"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -89,15 +90,69 @@ func (c *CompressResponseWriter) DetectCompressionType(req *Request, resp *Respo
 	if Config.BoolDefault("results.compressed", false) {
 		acceptedEncodings := strings.Split(req.Request.Header.Get("Accept-Encoding"), ",")
 
-		for i := 0; i < len(acceptedEncodings) && c.compressionType == ""; i++ {
-			switch strings.TrimSpace(acceptedEncodings[i]) {
-			case "gzip":
-				c.compressionType = "gzip"
-				c.compressWriter = gzip.NewWriter(resp.Out)
-			case "deflate":
-				c.compressionType = "deflate"
-				c.compressWriter = zlib.NewWriter(resp.Out)
+		largestQ := 0.0
+		chosenEncoding := len(compressionTypes)
+
+		for _, encoding := range acceptedEncodings {
+			encoding = strings.TrimSpace(encoding)
+			println(encoding)
+			encodingParts := strings.SplitN(encoding, ";", 2)
+
+			// If we are the format "gzip;q=0.8"
+			if len(encodingParts) > 1 {
+				// Strip off the q=
+				num, err := strconv.ParseFloat(strings.TrimSpace(encodingParts[1])[2:], 32)
+				if err != nil {
+					continue
+				}
+
+				if num >= largestQ && num > 0 {
+					if encodingParts[0] == "*" {
+						chosenEncoding = 0
+						largestQ = num
+						continue
+					}
+					for i, encoding := range compressionTypes {
+						if encoding == encodingParts[0] {
+							if i < chosenEncoding {
+								largestQ = num
+								chosenEncoding = i
+							}
+							break
+						}
+					}
+				}
+			} else {
+				// If we can accept anything, chose our preferred method.
+				if encodingParts[0] == "*" {
+					chosenEncoding = 0
+					largestQ = 1
+					break
+				}
+				// This is for just plain "gzip"
+				for i, encoding := range compressionTypes {
+					if encoding == encodingParts[0] {
+						if i < chosenEncoding {
+							largestQ = 1.0
+							chosenEncoding = i
+						}
+						break
+					}
+				}
 			}
+		}
+
+		if largestQ == 0 {
+			return
+		}
+
+		c.compressionType = compressionTypes[chosenEncoding]
+
+		switch c.compressionType {
+		case "gzip":
+			c.compressWriter = gzip.NewWriter(resp.Out)
+		case "deflate":
+			c.compressWriter = zlib.NewWriter(resp.Out)
 		}
 	}
 }
