017236c6856e3c0172340cb7d5f69229396e18cc
verdverm <verdverm@gmail.com>
2014-01-18 17:56:56 -0500

    adding documentation for OnAppStart(), also updating samples/booking to match the recommendation in docs

--- samples/booking/app/init.go
+++ samples/booking/app/init.go
@@ -18,6 +18,14 @@ func init() {
 		revel.CompressFilter,          // Compress the results.
 		revel.ActionInvoker,           // Invoke the action.
 	}
+
+	revel.OnAppStart(InitDB)
+	revel.InterceptMethod((*GorpController).Begin, revel.BEFORE)
+	revel.InterceptMethod(Application.AddUser, revel.BEFORE)
+	revel.InterceptMethod(Hotels.checkUser, revel.BEFORE)
+	revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
+	revel.InterceptMethod((*GorpController).Rollback, revel.FINALLY)
+
 }
 
 var HeaderFilter = func(c *revel.Controller, fc []revel.Filter) {
