9ffd5ebc2ced9e52c9dc67bd54e0a00b0189a950
Justin Li <jli.justinli@gmail.com>
2014-03-01 20:57:42 -0500

    Run go fmt

--- harness/build.go
+++ harness/build.go
@@ -130,7 +130,7 @@ func getAppVersion() string {
 		if (err != nil && os.IsNotExist(err)) || !info.IsDir() {
 			return ""
 		}
-		gitCmd := exec.Command(gitPath, "--git-dir=" + gitDir, "describe", "--always", "--dirty")
+		gitCmd := exec.Command(gitPath, "--git-dir="+gitDir, "describe", "--always", "--dirty")
 		revel.TRACE.Println("Exec:", gitCmd.Args)
 		output, err := gitCmd.Output()
 
