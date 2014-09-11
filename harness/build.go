57af6d3a2a7d551f914c8f8b6b04cb2fa5b87975
Robert O'Leary <robert@teamwork.com>
2014-09-11 11:04:32 -0400

    Better error parsing and using https://github.com/ktunkiewicz/subl-handler

--- harness/build.go
+++ harness/build.go
@@ -2,7 +2,6 @@ package harness
 
 import (
 	"fmt"
-	"github.com/revel/revel"
 	"go/build"
 	"os"
 	"os/exec"
@@ -13,6 +12,8 @@ import (
 	"strconv"
 	"strings"
 	"text/template"
+
+	"github.com/revel/revel"
 )
 
 var importErrorPattern = regexp.MustCompile("cannot find package \"([^\"]+)\"")
@@ -251,12 +252,20 @@ func newCompileError(output []byte) *revel.Error {
 	errorMatch := regexp.MustCompile(`(?m)^([^:#]+):(\d+):(\d+:)? (.*)$`).
 		FindSubmatch(output)
 	if errorMatch == nil {
-		revel.ERROR.Println("Failed to parse build errors:\n", string(output))
-		return &revel.Error{
-			SourceType:  "Go code",
-			Title:       "Go Compilation Error",
-			Description: "See console for build error.",
+		errorMatch = regexp.MustCompile(`(?m)^(.*?)\:(\d+)\:\s(.*?)$`).FindSubmatch(output)
+
+		if errorMatch == nil {
+			revel.ERROR.Println("Failed to parse build errors:\n", string(output))
+			return &revel.Error{
+				SourceType:  "Go code",
+				Title:       "Go Compilation Error",
+				Description: "See console for build error.",
+			}
 		}
+
+		errorMatch = append(errorMatch, errorMatch[3])
+
+		revel.ERROR.Println("Build errors:\n", string(output))
 	}
 
 	// Read the source for the offending file.
