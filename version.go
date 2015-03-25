83c62046c726f36daec7a28a335521f94b2561a6
Brenden Soares <bsoares@gmail.com>
2015-03-25 16:35:09 -0700

    Updates version number and build date

--- version.go
+++ version.go
@@ -2,9 +2,9 @@ package revel
 
 const (
 	// Current Revel version
-	VERSION    = "0.12.0dev"
+	VERSION    = "0.12.0"
 	// Latest commit date
-	BUILD_DATE = "2015-01-19"
+	BUILD_DATE = "2015-03-25"
 	// Minimum required Go version
 	MINIMUM_GO = ">= go1.3"
 )
