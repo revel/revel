8b0fee737d03d09b31b7c29ff9f68a400c013b09
Jeevanandam M <jeeva@myjeeva.com>
2016-05-24 23:01:05 -0700

    #756 defer close call for io.Closer

--- invoker.go
+++ invoker.go
@@ -1,6 +1,7 @@
 package revel
 
 import (
+	"io"
 	"reflect"
 
 	"golang.org/x/net/websocket"
@@ -25,6 +26,11 @@ func ActionInvoker(c *Controller, _ []Filter) {
 			boundArg = reflect.ValueOf(c.Request.Websocket)
 		} else {
 			boundArg = Bind(c.Params, arg.Name, arg.Type)
+			// #756 - If the argument is a closer, defer a Close call,
+			// so we don't risk on leaks.
+			if closer, ok := boundArg.Interface().(io.Closer); ok {
+				defer closer.Close()
+			}
 		}
 		methodArgs = append(methodArgs, boundArg)
 	}
