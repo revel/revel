64281f9c6148b6c1c0c84de2f327c5f28576c905
Jia Lu <lujiacn@gmail.com>
2016-09-18 12:28:59 +0800

    changed default CookieSecure to false

--- revel.go
+++ revel.go
@@ -204,7 +204,7 @@ func Init(mode, importPath, srcPath string) {
 	AppRoot = Config.StringDefault("app.root", "")
 	CookiePrefix = Config.StringDefault("cookie.prefix", "REVEL")
 	CookieDomain = Config.StringDefault("cookie.domain", "")
-	CookieSecure = Config.BoolDefault("cookie.secure", !DevMode)
+	CookieSecure = Config.BoolDefault("cookie.secure", false)
 	TemplateDelims = Config.StringDefault("template.delimiters", "")
 	if secretStr := Config.StringDefault("app.secret", ""); secretStr != "" {
 		secretKey = []byte(secretStr)
