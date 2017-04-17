0171a2341bf4898e317648deb83445342de4b81a
yangfei <yangfei86@vip.qq.com>
2017-04-17 17:11:27 +0800

    receive a post method of json request.

--- controller.go
+++ controller.go
@@ -5,9 +5,11 @@
 package revel
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -132,6 +134,22 @@ func (c *Controller) RenderTemplate(templatePath string) Result {
 	return &RenderTemplateResult{
 		Template:   template,
 		ViewArgs: c.ViewArgs,
+	}
+}
+
+//receive a post method of json request.
+//WARNING: the arg must be &receive
+func (c *Controller) ReceiveJSON(receive interface{}) error {
+	if strings.Contains(c.Request.ContentType, "application/json") {
+		content, _ := ioutil.ReadAll(c.Request.Body)
+		err := json.Unmarshal([]byte(content), &receive)
+		if err != nil {
+			return err
+		} else {
+			return nil
+		}
+	} else {
+		return errors.New("It's not a JSON request!")
 	}
 }
 
