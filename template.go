c243a7aa0cfc25251a3029a1f5cff60b70274f0b
Ari Seyhun <ariseyhun@live.com.au>
2017-06-03 16:40:07 +0930

    Add template func IsEmpty

--- template.go
+++ template.go
@@ -63,6 +63,9 @@ var (
 			}
 			return template.JS("")
 		},
+		"isEmpty": func(v interface{}) bool {
+			return IsEmpty(reflect.ValueOf(v))
+		},
 		"field": NewField,
 		"firstof": func(args ...interface{}) interface{} {
 			for _, val := range args {
@@ -484,4 +487,28 @@ func Slug(text string) string {
 	text = whiteSpacePattern.ReplaceAllString(text, separator)
 	text = strings.Trim(text, separator)
 	return text
+}
+
+// IsEmpty checks if an interface is zero.
+// For example, an empty string or an int = 0 will return true.
+func IsEmpty(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Func, reflect.Map, reflect.Slice:
+		return v.IsNil()
+	case reflect.Array:
+		z := true
+		for i := 0; i < v.Len(); i++ {
+			z = z && isZero(v.Index(i))
+		}
+		return z
+	case reflect.Struct:
+		z := true
+		for i := 0; i < v.NumField(); i++ {
+			z = z && isZero(v.Field(i))
+		}
+		return z
+	}
+	// Compare other types directly:
+	z := reflect.Zero(v.Type())
+	return v.Interface() == z.Interface()
 }
