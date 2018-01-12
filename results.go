revel/revel

commit 75b7d91193e7ecfa42c6d0c66113beff684976e8

Added args to be passed to action using the Controller.Redirect Added namespace to resolved path Renamed ServerHeader to Header (more backwards compatible) Updated intercept module to sort intercepts by "depth" this ensures that the most embedded intercept gets called first Added "appModule" to revel.Modules Added additional logging in router.splitactionpath Added setting template initialization engines to be read from template.engines

results.go
@@ -432,10 +432,11 @@ func (r *RedirectToURLResult) Apply(req *Request, resp *Response) {
 
 type RedirectToActionResult struct {
 	val interface{}
+	args []interface{}
 }
 
 func (r *RedirectToActionResult) Apply(req *Request, resp *Response) {
-	url, err := getRedirectURL(r.val)
+	url, err := getRedirectURL(r.val, r.args)
 	if err != nil {
 		resultsLog.Error("Apply: Couldn't resolve redirect", "error", err)
 		ErrorResult{Error: err}.Apply(req, resp)
@@ -445,7 +446,7 @@ func (r *RedirectToActionResult) Apply(req *Request, resp *Response) {
 	resp.WriteHeader(http.StatusFound, "")
 }
 
-func getRedirectURL(item interface{}) (string, error) {
+func getRedirectURL(item interface{}, args []interface{}) (string, error) {
 	// Handle strings
 	if url, ok := item.(string); ok {
 		return url, nil
@@ -466,8 +467,28 @@ func getRedirectURL(item interface{}) (string, error) {
 		if recvType.Kind() == reflect.Ptr {
 			recvType = recvType.Elem()
 		}
-		action := recvType.Name() + "." + method.Name
-		actionDef := MainRouter.Reverse(action, make(map[string]string))
+		module := ModuleFromPath(recvType.PkgPath(),true)
+		println("Returned ", module)
+		action := module.Namespace() + recvType.Name() + "." + method.Name
+		// Fetch the action path to get the defaults
+		pathData, found := splitActionPath(nil, action, true)
+		if !found {
+			return "", fmt.Errorf("Unable to redirect '%s', expected 'Controller.Action'", action)
+		}
+
+		// Build the map for the router to reverse
+		// Unbind the arguments.
+		argsByName := make(map[string]string)
+		// Bind any static args first
+		fixedParams := len(pathData.FixedParamsByName)
+		methodType := pathData.TypeOfController.Method(pathData.MethodName)
+
+		for i, argValue := range args {
+			Unbind(argsByName, methodType.Args[i+fixedParams].Name, argValue)
+		}
+
+
+		actionDef := MainRouter.Reverse(action, argsByName)
 		if actionDef == nil {
 			return "", errors.New("no route for action " + action)
 		}

controller.go
@@ -329,7 +329,7 @@ func (c *Controller) Redirect(val interface{}, args ...interface{}) Result {
 		}
 		return &RedirectToURLResult{fmt.Sprintf(url, args...)}
 	}
-	return &RedirectToActionResult{val}
+	return &RedirectToActionResult{val, args}
 }
 
 // This stats returns some interesting stats based on what is cached in memory

http.go
@@ -22,7 +22,7 @@ import (
 // Request is Revel's HTTP request object structure
 type Request struct {
 	In              ServerRequest
-	ServerHeader    *RevelHeader
+	Header          *RevelHeader
 	ContentType     string
 	Format          string // "html", "xml", "json", or "txt"
 	AcceptLanguages AcceptLanguages
@@ -69,7 +69,7 @@ func NewResponse(w ServerResponse) (r *Response) {
 
 // NewRequest returns a Revel's HTTP request instance with given HTTP instance
 func NewRequest(r ServerRequest) *Request {
-	req := &Request{ServerHeader: &RevelHeader{}}
+	req := &Request{Header: &RevelHeader{}}
 	if r != nil {
 		req.SetRequest(r)
 	}
@@ -78,7 +78,7 @@ func NewRequest(r ServerRequest) *Request {
 func (req *Request) SetRequest(r ServerRequest) {
 	req.In = r
 	if h, e := req.In.Get(HTTP_SERVER_HEADER); e == nil {
-		req.ServerHeader.Server = h.(ServerHeader)
+		req.Header.Server = h.(ServerHeader)
 	}
 
 	req.URL,_ = req.GetValue(HTTP_URL).(*url.URL)
@@ -91,8 +91,8 @@ func (req *Request) SetRequest(r ServerRequest) {
 
 }
 func (req *Request) Cookie(key string) (ServerCookie, error) {
-	if req.ServerHeader.Server != nil {
-		return req.ServerHeader.Server.GetCookie(key)
+	if req.Header.Server != nil {
+		return req.Header.Server.GetCookie(key)
 	}
 	return nil, http.ErrNoCookie
 }
@@ -193,7 +193,7 @@ func (req *Request) Destroy() {
 	req.Method = ""
 	req.RemoteAddr = ""
 	req.Host = ""
-	req.ServerHeader.Destroy()
+	req.Header.Destroy()
 	req.URL = nil
 	req.Form = nil
 	req.MultipartForm = nil
@@ -221,15 +221,15 @@ func (resp *Response) Destroy() {
 
 // UserAgent returns the client's User-Agent header string.
 func (r *Request) UserAgent() string {
-	return r.ServerHeader.Get("User-Agent")
+	return r.Header.Get("User-Agent")
 }
 
 // Referer returns the client's Referer header string.
 func (req *Request) Referer() string {
-	return req.ServerHeader.Get("Referer")
+	return req.Header.Get("Referer")
 }
 func (req *Request) GetHttpHeader(key string) string {
-	return req.ServerHeader.Get(key)
+	return req.Header.Get(key)
 }
 
 func (r *Request) GetValue(key int) (value interface{}) {
@@ -329,7 +329,7 @@ func (h *RevelHeader) GetAll(key string) (values []string) {
 // If none is specified, returns "text/html" by default.
 func ResolveContentType(req *Request) string {
 
-	contentType := req.ServerHeader.Get("Content-Type")
+	contentType := req.Header.Get("Content-Type")
 	if contentType == "" {
 		return "text/html"
 	}
@@ -412,7 +412,7 @@ func (al AcceptLanguages) String() string {
 // See the HTTP header fields specification
 // (http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4) for more details.
 func ResolveAcceptLanguage(req *Request) AcceptLanguages {
-	header := req.ServerHeader.Get("Accept-Language")
+	header := req.Header.Get("Accept-Language")
 	if header == "" {
 		return req.AcceptLanguages
 	}

intercept.go
@@ -7,6 +7,7 @@ package revel
 import (
 	"log"
 	"reflect"
+	"sort"
 )
 
 // An InterceptorFunc is functionality invoked by the framework BEFORE or AFTER
@@ -68,11 +69,11 @@ type Interception struct {
 
 // Invoke performs the given interception.
 // val is a pointer to the App Controller.
-func (i Interception) Invoke(val reflect.Value) reflect.Value {
+func (i Interception) Invoke(val reflect.Value, target *reflect.Value) reflect.Value {
 	var arg reflect.Value
 	if i.function == nil {
 		// If it's an InterceptorMethod, then we have to pass in the target type.
-		arg = findTarget(val, i.target)
+		arg = *target
 	} else {
 		// If it's an InterceptorFunc, then the type must be *Controller.
 		// We can find that by following the embedded types up the chain.
@@ -115,7 +116,7 @@ func invokeInterceptors(when When, c *Controller) {
 	)
 
 	for _, intc := range getInterceptors(when, app) {
-		resultValue := intc.Invoke(app)
+		resultValue := intc.Interceptor.Invoke(app, &intc.Target)
 		if !resultValue.IsNil() {
 			result = resultValue.Interface().(Result)
 		}
@@ -154,6 +155,7 @@ func InterceptMethod(intc InterceptorMethod, when When) {
 		log.Fatalln("Interceptor method should have signature like",
 			"'func (c *AppController) example() revel.Result' but was", methodType)
 	}
+
 	interceptors = append(interceptors, &Interception{
 		When:     when,
 		method:   intc,
@@ -162,16 +164,40 @@ func InterceptMethod(intc InterceptorMethod, when When) {
 	})
 }
 
-func getInterceptors(when When, val reflect.Value) []*Interception {
-	result := []*Interception{}
+// This item is used to provide a sortable set to be returned to the caller. This ensures calls order is maintained
+//
+type interceptorItem struct {
+	Interceptor *Interception
+	Target reflect.Value
+	Level int
+}
+type interceptorItemList []*interceptorItem
+func (a interceptorItemList) Len() int           { return len(a) }
+func (a interceptorItemList) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a interceptorItemList) Less(i, j int) bool { return a[i].Level < a[j].Level }
+type reverseInterceptorItemList []*interceptorItem
+func (a reverseInterceptorItemList) Len() int           { return len(a) }
+func (a reverseInterceptorItemList) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a reverseInterceptorItemList) Less(i, j int) bool { return a[i].Level > a[j].Level }
+func getInterceptors(when When, val reflect.Value) interceptorItemList {
+	result := interceptorItemList{}
 	for _, intc := range interceptors {
 		if intc.When != when {
 			continue
 		}
 
-		if intc.interceptAll || findTarget(val, intc.target).IsValid() {
-			result = append(result, intc)
+		level,target := findTarget(val, intc.target)
+		if intc.interceptAll || target.IsValid() {
+			result = append(result, &interceptorItem{intc, target, level})
 		}
+	}
+
+	// Before is deepest to highest
+	if when ==BEFORE {
+		sort.Sort(result)
+	} else {
+		// Everything else is highest to deepest
+		sort.Sort(reverseInterceptorItemList(result))
 	}
 	return result
 }
@@ -179,21 +205,22 @@ func getInterceptors(when When, val reflect.Value) []*Interception {
 // Find the value of the target, starting from val and including embedded types.
 // Also, convert between any difference in indirection.
 // If the target couldn't be found, the returned Value will have IsValid() == false
-func findTarget(val reflect.Value, target reflect.Type) reflect.Value {
+func findTarget(val reflect.Value, target reflect.Type) (int, reflect.Value) {
 	// Look through the embedded types (until we reach the *revel.Controller at the top).
 	valueQueue := []reflect.Value{val}
+	level := 0
 	for len(valueQueue) > 0 {
 		val, valueQueue = valueQueue[0], valueQueue[1:]
 
 		// Check if val is of a similar type to the target type.
 		if val.Type() == target {
-			return val
+			return level,val
 		}
 		if val.Kind() == reflect.Ptr && val.Elem().Type() == target {
-			return val.Elem()
+			return level,val.Elem()
 		}
 		if target.Kind() == reflect.Ptr && target.Elem() == val.Type() {
-			return val.Addr()
+			return level,val.Addr()
 		}
 
 		// If we reached the *revel.Controller and still didn't find what we were
@@ -212,7 +239,8 @@ func findTarget(val reflect.Value, target reflect.Type) reflect.Value {
 				valueQueue = append(valueQueue, val.Field(i))
 			}
 		}
+		level --
 	}
 
-	return reflect.Value{}
+	return level, reflect.Value{}
 }

intercept_test.go
@@ -77,8 +77,8 @@ func testInterceptorController(t *testing.T, appControllerPtr reflect.Value, met
 	}
 }
 
-func testInterception(t *testing.T, intc *Interception, arg reflect.Value) {
-	val := intc.Invoke(arg)
+func testInterception(t *testing.T, intc *interceptorItem, arg reflect.Value) {
+	val := intc.Interceptor.Invoke(arg, &intc.Target)
 	if !val.IsNil() {
 		t.Errorf("Failed (%v): Expected nil got %v", intc, val)
 	}

module.go
@@ -47,8 +47,10 @@ func RegisterModuleInit(callback ModuleCallbackInterface) {
 func init() {
 	AddInitEventHandler(func(typeOf int, value interface{}) (responseOf int) {
 		if typeOf == REVEL_BEFORE_MODULES_LOADED {
-			Modules = []*Module{}
+			Modules = []*Module{appModule}
+			appModule.Path = AppPath
 		}
+
 		return
 	})
 }
@@ -83,7 +85,7 @@ func (m *Module) AddController(ct *ControllerType) {
 // Only to be used on initialization
 func ModuleFromPath(path string, addGopathToPath bool) (module *Module) {
 	gopathList := filepath.SplitList(build.Default.GOPATH)
-	// ignore the vendor folder
+	// Strip away the vendor folder
 	if i := strings.Index(path, "/vendor/"); i > 0 {
 		path = path[i+len("vendor/"):]
 	}

router.go
@@ -204,7 +204,11 @@ func (router *Router) Route(req *Request) (routeMatch *RouteMatch) {
 
 		// If wildcard match on method name use the method name from the params
 		if methodName[0] == ':' {
-			methodName = strings.ToLower(params[methodName[1:]][0])
+			if methodKey, found := params[methodName[1:]]; found && len(methodKey) > 0 {
+				methodName = strings.ToLower(methodKey[0])
+			} else {
+				routerLog.Fatal("Route: Failure to find method name in parameters", "params", params, "methodName", methodName)
+			}
 		}
 
 		// If the action is variablized, replace into it with the captured args.
@@ -294,6 +298,7 @@ func splitActionPath(actionPathData *ActionPathData, actionPath string, useCache
 		controllerNamespace, controllerName, methodName, action string
 		foundModuleSource                                       *Module
 		typeOfController                                        *ControllerType
+		log = routerLog.New("actionPath",actionPath)
 	)
 	actionSplit := strings.Split(actionPath, ".")
 	if actionPathData != nil {
@@ -304,14 +309,16 @@ func splitActionPath(actionPathData *ActionPathData, actionPath string, useCache
 		if i := strings.Index(methodName, "("); i > 0 {
 			methodName = methodName[:i]
 		}
-		// INFO.Println("Check", actionPath, controllerName,methodName)
+		log = log.New("controller", controllerName, "method",methodName)
+		log.Debug("splitActionPath: Check for namespace",)
 		if i := strings.Index(controllerName, namespaceSeperator); i > -1 {
 			controllerNamespace = controllerName[:i+1]
 			if moduleSource, found := ModuleByName(controllerNamespace[:len(controllerNamespace)-1]); found {
+				log.Debug("Found module namespace")
 				foundModuleSource = moduleSource
 				controllerNamespace = moduleSource.Namespace()
 			} else {
-				routerLog.Warnf("splitActionPath: Unable to find module %s for action: %s", controllerNamespace[:len(controllerNamespace)-1], actionPath)
+				log.Warnf("splitActionPath: Unable to find module %s for action: %s", controllerNamespace[:len(controllerNamespace)-1], actionPath)
 			}
 			controllerName = controllerName[i+1:]
 			// Check for the type of controller
@@ -325,7 +332,7 @@ func splitActionPath(actionPathData *ActionPathData, actionPath string, useCache
 					controllerNamespace = typeOfController.Namespace
 				}
 			}
-			// INFO.Println("Check for controller by module", typeOfController)
+			log.Info("Found controller for path", "controllerType", typeOfController)
 
 			if typeOfController == nil {
 				// Check to see if we can determine the controller from only the controller name
@@ -363,7 +370,7 @@ func splitActionPath(actionPathData *ActionPathData, actionPath string, useCache
 		for path := range actionPathCacheMap {
 			foundPaths += path + ","
 		}
-		routerLog.Warnf("splitActionPath: Invalid action path %s found paths %s", actionPath, foundPaths)
+		log.Warnf("splitActionPath: Invalid action path %s found paths %s", actionPath, foundPaths)
 		found = false
 	}
 
@@ -390,7 +397,7 @@ func splitActionPath(actionPathData *ActionPathData, actionPath string, useCache
 		}
 		actionPathData.TypeOfController = foundModuleSource.ControllerByName(controllerName, "")
 		if actionPathData.TypeOfController == nil && actionPathData.ControllerName[0] != ':' {
-			routerLog.Warnf("splitActionPath: No controller found for %s %#v", foundModuleSource.Namespace()+controllerName, controllers)
+			log.Warnf("splitActionPath: No controller found for %s %#v", foundModuleSource.Namespace()+controllerName, controllers)
 		}
 
 		pathData = actionPathData
@@ -404,10 +411,14 @@ func splitActionPath(actionPathData *ActionPathData, actionPath string, useCache
 			actionPath = actionPath + "(" + strings.ToLower(strings.Join(pathData.Route.FixedParams, ",")) + ")"
 		}
 		if actionPathData.Route != nil {
-			routerLog.Debugf("splitActionPath: Split Storing recognized action path %s for route  %#v ", actionPath, actionPathData.Route)
+			log.Debugf("splitActionPath: Split Storing recognized action path %s for route  %#v ", actionPath, actionPathData.Route)
 		}
 		pathData.Key = actionPath
 		actionPathCacheMap[actionPath] = pathData
+		if !strings.Contains(actionPath,namespaceSeperator) && pathData.TypeOfController!=nil {
+			actionPathCacheMap[strings.ToLower(pathData.TypeOfController.Namespace) + actionPath] = pathData
+			log.Debugf("splitActionPath: Split Storing recognized action path %s for route  %#v ", strings.ToLower(pathData.TypeOfController.Namespace) + actionPath, actionPathData.Route)
+		}
 	}
 	return
 }
@@ -602,128 +613,133 @@ func (a *ActionDefinition) String() string {
 }
 
 func (router *Router) Reverse(action string, argValues map[string]string) (ad *ActionDefinition) {
+	log := routerLog.New("action",action)
 	pathData, found := splitActionPath(nil, action, true)
+	if !found {
+		routerLog.Error("splitActionPath: Failed to find reverse route", "action", action, "arguments", argValues)
+		return nil
+	}
 
-	if found {
-		if pathData.Route == nil {
-			var possibleRoute *Route
-			// If the route is nil then we need to go through the routes to find the first matching route
-			// from this controllers namespace, this is likely a wildcard route match
-			for _, route := range router.Routes {
-				// Skip routes that are not wild card or empty
-				if route.ControllerName == "" || route.MethodName == "" {
-					continue
-				}
-				if route.ModuleSource == pathData.ModuleSource && route.ControllerName[0] == ':' {
-					// Wildcard match in same module space
-					pathData.Route = route
-					break
-				} else if route.ActionPath() == pathData.ModuleSource.Namespace()+pathData.ControllerName &&
-					(route.Method[0] == ':' || route.Method == pathData.MethodName) {
-					// Action path match
-					pathData.Route = route
-					break
-				} else if route.ControllerName == pathData.ControllerName &&
-					(route.Method[0] == ':' || route.Method == pathData.MethodName) {
-					// Controller name match
-					possibleRoute = route
-				}
-			}
-			if pathData.Route == nil && possibleRoute != nil {
-				pathData.Route = possibleRoute
-				routerLog.Warnf("Reverse: For a url reverse a match was based on  %s matched path to route %#v ", action, possibleRoute)
-			}
-			if pathData.Route != nil {
-				routerLog.Debugf("Reverse: Reverse Storing recognized action path %s for route %#v\n", action, pathData.Route)
-			}
-		}
-
-		// Likely unknown route because of a wildcard, perform manual lookup
-		if pathData.Route != nil {
-			route := pathData.Route
-
-			// If the controller or method are wildcards we need to populate the argValues
-			controllerWildcard := route.ControllerName[0] == ':'
-			methodWildcard := route.MethodName[0] == ':'
-
-			// populate route arguments with the names
-			if controllerWildcard {
-				argValues[route.ControllerName[1:]] = pathData.ControllerName
-			}
-			if methodWildcard {
-				argValues[route.MethodName[1:]] = pathData.MethodName
-			}
-			// In theory all routes should be defined and pre-populated, the route controllers may not be though
-			// with wildcard routes
-			if pathData.TypeOfController == nil {
-				if controllerWildcard || methodWildcard {
-					if controller := ControllerTypeByName(pathData.ControllerNamespace+pathData.ControllerName, route.ModuleSource); controller != nil {
-						// Wildcard match boundary
-						pathData.TypeOfController = controller
-						// See if the path exists in the module based
-					} else {
-						routerLog.Errorf("Reverse: Controller %s not found in reverse lookup", pathData.ControllerNamespace+pathData.ControllerName)
-						return
-					}
-				}
-			}
-
-			if pathData.TypeOfController == nil {
-				routerLog.Errorf("Reverse: Controller %s not found in reverse lookup", pathData.ControllerNamespace+pathData.ControllerName)
-				return
-			}
-			var (
-				queryValues  = make(url.Values)
-				pathElements = strings.Split(route.Path, "/")
-			)
-			for i, el := range pathElements {
-				if el == "" || (el[0] != ':' && el[0] != '*') {
-					continue
-				}
-				val, ok := pathData.FixedParamsByName[el[1:]]
-				if !ok {
-					val, ok = argValues[el[1:]]
-				}
-				if !ok {
-					val = "<nil>"
-					routerLog.Error("Reverse: reverse route missing route argument ", "argument", el[1:])
-				}
-				pathElements[i] = val
-				delete(argValues, el[1:])
+	log.Debug("Checking for route","pathdataRoute",pathData.Route)
+	if pathData.Route == nil {
+		var possibleRoute *Route
+		// If the route is nil then we need to go through the routes to find the first matching route
+		// from this controllers namespace, this is likely a wildcard route match
+		for _, route := range router.Routes {
+			// Skip routes that are not wild card or empty
+			if route.ControllerName == "" || route.MethodName == "" {
 				continue
 			}
-
-			// Add any args that were not inserted into the path into the query string.
-			for k, v := range argValues {
-				queryValues.Set(k, v)
-			}
-
-			// Calculate the final URL and Method
-			urlPath := strings.Join(pathElements, "/")
-			if len(queryValues) > 0 {
-				urlPath += "?" + queryValues.Encode()
-			}
-
-			method := route.Method
-			star := false
-			if route.Method == "*" {
-				method = "GET"
-				star = true
-			}
-
-			//INFO.Printf("Reversing action %s to %s Using Route %#v",action,url,pathData.Route)
-
-			return &ActionDefinition{
-				URL:    urlPath,
-				Method: method,
-				Star:   star,
-				Action: action,
-				Args:   argValues,
-				Host:   "TODO",
+			if route.ModuleSource == pathData.ModuleSource && route.ControllerName[0] == ':' {
+				// Wildcard match in same module space
+				pathData.Route = route
+				break
+			} else if route.ActionPath() == pathData.ModuleSource.Namespace()+pathData.ControllerName &&
+				(route.Method[0] == ':' || route.Method == pathData.MethodName) {
+				// Action path match
+				pathData.Route = route
+				break
+			} else if route.ControllerName == pathData.ControllerName &&
+				(route.Method[0] == ':' || route.Method == pathData.MethodName) {
+				// Controller name match
+				possibleRoute = route
 			}
 		}
+		if pathData.Route == nil && possibleRoute != nil {
+			pathData.Route = possibleRoute
+			routerLog.Warnf("Reverse: For a url reverse a match was based on  %s matched path to route %#v ", action, possibleRoute)
+		}
+		if pathData.Route != nil {
+			routerLog.Debugf("Reverse: Reverse Storing recognized action path %s for route %#v\n", action, pathData.Route)
+		}
 	}
-	routerLog.Error("Reverse: Failed to find reverse route", "action", action, "arguments", argValues)
+
+	// Likely unknown route because of a wildcard, perform manual lookup
+	if pathData.Route != nil {
+		route := pathData.Route
+
+		// If the controller or method are wildcards we need to populate the argValues
+		controllerWildcard := route.ControllerName[0] == ':'
+		methodWildcard := route.MethodName[0] == ':'
+
+		// populate route arguments with the names
+		if controllerWildcard {
+			argValues[route.ControllerName[1:]] = pathData.ControllerName
+		}
+		if methodWildcard {
+			argValues[route.MethodName[1:]] = pathData.MethodName
+		}
+		// In theory all routes should be defined and pre-populated, the route controllers may not be though
+		// with wildcard routes
+		if pathData.TypeOfController == nil {
+			if controllerWildcard || methodWildcard {
+				if controller := ControllerTypeByName(pathData.ControllerNamespace+pathData.ControllerName, route.ModuleSource); controller != nil {
+					// Wildcard match boundary
+					pathData.TypeOfController = controller
+					// See if the path exists in the module based
+				} else {
+					routerLog.Errorf("Reverse: Controller %s not found in reverse lookup", pathData.ControllerNamespace+pathData.ControllerName)
+					return
+				}
+			}
+		}
+
+		if pathData.TypeOfController == nil {
+			routerLog.Errorf("Reverse: Controller %s not found in reverse lookup", pathData.ControllerNamespace+pathData.ControllerName)
+			return
+		}
+		var (
+			queryValues  = make(url.Values)
+			pathElements = strings.Split(route.Path, "/")
+		)
+		for i, el := range pathElements {
+			if el == "" || (el[0] != ':' && el[0] != '*') {
+				continue
+			}
+			val, ok := pathData.FixedParamsByName[el[1:]]
+			if !ok {
+				val, ok = argValues[el[1:]]
+			}
+			if !ok {
+				val = "<nil>"
+				routerLog.Error("Reverse: reverse route missing route argument ", "argument", el[1:])
+			}
+			pathElements[i] = val
+			delete(argValues, el[1:])
+			continue
+		}
+
+		// Add any args that were not inserted into the path into the query string.
+		for k, v := range argValues {
+			queryValues.Set(k, v)
+		}
+
+		// Calculate the final URL and Method
+		urlPath := strings.Join(pathElements, "/")
+		if len(queryValues) > 0 {
+			urlPath += "?" + queryValues.Encode()
+		}
+
+		method := route.Method
+		star := false
+		if route.Method == "*" {
+			method = "GET"
+			star = true
+		}
+
+		//INFO.Printf("Reversing action %s to %s Using Route %#v",action,url,pathData.Route)
+
+		return &ActionDefinition{
+			URL:    urlPath,
+			Method: method,
+			Star:   star,
+			Action: action,
+			Args:   argValues,
+			Host:   "TODO",
+		}
+	}
+
+	routerLog.Error("Reverse: Failed to find controller for reverse route", "action", action, "arguments", argValues)
 	return nil
 }
 
@@ -750,11 +766,10 @@ func RouterFilter(c *Controller, fc []Filter) {
 	// Add the route and fixed params to the Request Params.
 	c.Params.Route = route.Params
 	// Assign logger if from module
-	if c.Type.ModuleSource!=nil && c.Type.ModuleSource!=appModule {
+	if c.Type.ModuleSource != nil && c.Type.ModuleSource != appModule {
 		c.Log = c.Type.ModuleSource.Log.New("ip", c.ClientIP,
 			"path", c.Request.URL.Path, "method", c.Request.Method)
 	}
-
 
 	// Add the fixed parameters mapped by name.
 	// TODO: Pre-calculate this mapping.

template.go
@@ -114,7 +114,7 @@ func (loader *TemplateLoader) Refresh() (err *Error) {
 		templateMap: map[string]Template{}}
 
 	templateLog.Debug("Refresh: Refreshing templates from ", "path", loader.paths)
-	if err = loader.initializeEngines(runtimeLoader, GO_TEMPLATE); err != nil {
+	if err = loader.initializeEngines(runtimeLoader, Config.StringDefault("template.engines", GO_TEMPLATE)); err != nil {
 		return
 	}
 	for _, engine := range runtimeLoader.templatesAndEngineList {

commit 3c288028205b4b0155e5a2c224a561a1842d6f3b

Fix issue revel/cmd#113
In some cases (especially during a redirect), module on line 470 returned from `ModuleFromPath()` returned `nil`, causing the application to crash after using `module.Namespace()`.

Commit notzippy@75b7d91 introduced this bug.

results.go
@@ -469,7 +469,13 @@ func getRedirectURL(item interface{}, args []interface{}) (string, error) {
 		}
 		module := ModuleFromPath(recvType.PkgPath(),true)
 		println("Returned ", module)
-		action := module.Namespace() + recvType.Name() + "." + method.Name
+		var moduleNamespace string
+		if module != nil {
+			moduleNamespace = module.Namespace()
+		} else {
+			moduleNamespace = recvType.Name()
+		}
+		action := moduleNamespace + recvType.Name() + "." + method.Name
 		// Fetch the action path to get the defaults
 		pathData, found := splitActionPath(nil, action, true)
 		if !found {