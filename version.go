fd65ef0defe5fa045524fd3319515793162a3628
NotZippy <NotZippy@gmail.com>
2018-09-23 11:18:55 -0700

    release v0.20.0

--- version.go
+++ version.go
@@ -6,10 +6,10 @@ package revel
 
 const (
 	// Version current Revel version
-	Version = "0.20.0-dev"
+	Version = "0.20.0"
 
 	// BuildDate latest commit/release date
-	BuildDate = "2018-02-06"
+	BuildDate = "2018-09-23"
 
 	// MinimumGoVersion minimum required Go version for Revel
 	MinimumGoVersion = ">= go1.8"
