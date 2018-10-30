a3d7a7c23ca885cc5036d3641ede49ce4a14ee2e
NotZippy <NotZippy@gmail.com>
2018-10-30 06:23:52 -0700

    release v0.21.0

--- version.go
+++ version.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2017 The Revel Framework Authors, All rights reserved.
+// Copyright (c) 2012-2018 The Revel Framework Authors, All rights reserved.
 // Revel Framework source code and usage is governed by a MIT style
 // license that can be found in the LICENSE file.
 
@@ -6,10 +6,10 @@ package revel
 
 const (
 	// Version current Revel version
-	Version = "0.21.0-dev"
+	Version = "0.21.0"
 
 	// BuildDate latest commit/release date
-	BuildDate = "2018-09-23"
+	BuildDate = "2018-10-30"
 
 	// MinimumGoVersion minimum required Go version for Revel
 	MinimumGoVersion = ">= go1.8"
